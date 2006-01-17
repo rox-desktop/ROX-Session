1ebcccd85fe4968ce3b8beef79e29fdb8241c8d2
Stephen Watson <stephen@kerofin.demon.co.uk>
2006-01-17 11:24:14 +0000

    Use correct interface for settings (Stephen Watson).
    git-svn-id: svn://svn.code.sf.net/p/rox/svn/trunk/ROX-Session@4314 66de3db3-b00d-0410-b41b-f4738ad19bea

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 site = 'rox.sourceforge.net'
 session_service = "net.sf.rox.Session"
 control_interface = 'net.sf.rox.Session.Control'
-settings_interface = 'net.sf.rox.Session.Control'
+settings_interface = 'net.sf.rox.Session.Settings'
