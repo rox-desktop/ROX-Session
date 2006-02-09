f53518b985d6aad5eae19472c14b013d4ec7cc86
Stephen Watson <stephen@kerofin.demon.co.uk>
2006-02-09 19:09:56 +0000

    More work on setting up the user's environment (Stephen Watson).
    git-svn-id: svn://svn.code.sf.net/p/rox/svn/trunk/ROX-Session@4350 66de3db3-b00d-0410-b41b-f4738ad19bea

--- Environment.py
+++ Environment.py
@@ -11,32 +11,58 @@ session."""
 
 import os
 
+home=os.getenv('HOME')
+
 # Example: set up LD_LIBRARY_PATH
 #os.putenv('LD_LIBRARY_PATH',
-#          os.path.join(os.getenv('HOME'), 'lib')+
+#          os.path.join(home, 'lib')+
 #          os.getenv('LD_LIBRARY_PATH', '/usr/local/lib:/usr/lib')
 
 # Ensure ssh-agent is running
+def parse_ssh_agent_cache():
+    """If ~/.ssh-agent exits, read and parse it to get the ssh-agent
+    settings.  Used by start_ssh_agent()."""
+    ssh_cache=os.path.join(home, '.ssh-agent')
+    if not os.access(ssh_cache, os.R_OK):
+        return
+
+    vars={}
+    for line in file(ssh_cache, 'r'):
+        for cmd in line.strip().split(';'):
+            if '=' in cmd:
+                var, val=cmd.split('=', 1)
+                vars[var]=val
+
+            elif cmd.strip():
+                exp, var=cmd.strip().split(None, 1)
+                if exp=='export':
+                    try:
+                        os.environ[var]=vars[var]
+                    except:
+                        pass
+    
 def start_ssh_agent():
-    ssh_dir=os.path.join(os.getenv('HOME'), '.ssh')
-    if os.isdir(ssh_dir):
+    """Start the ssh-agent daemon if it is not running."""
+    ssh_dir=os.path.join(home, '.ssh')
+    if os.path.isdir(ssh_dir):
         if not os.getenv('SSH_AGENT_PID'):
-            ssh_cache=os.path.join(os.getenv('HOME'), '.ssh-agent')
+            try:
+                parse_ssh_agent_cache()
+            except:
+                pass
+
+        try:
+            sockname=os.environ['SSH_AUTH_SOCK']
+        except:
+            sockname=None
+
+        if not sockname or not os.access(sockname, os.R_OK):
+            ssh_cache=os.path.join(home, '.ssh-agent')
             os.system('ssh-agent -s > '+ssh_cache)
+            os.chmod(ssh_cache, 0600)
 
-            vars={}
-            for line in file(ssh_cache, 'r'):
-                for cmd in line.strip().split(';'):
-                    if '=' in cmd:
-                        var, val=cmd.split('=', 1)
-                        vars[var]=val
+            parse_ssh_agent_cache()
 
-                    else:
-                        exp, var=cmd.strip().split()
-                        if exp=='export':
-                            try:
-                                os.putenv(var, vars[var])
-                            except:
-                                pass
-
+# Uncomment this line to have ROX-Session start a ssh-agent daemon for
+# your session.  ssh-agent makes using ssh easier.
 #start_ssh_agent()
