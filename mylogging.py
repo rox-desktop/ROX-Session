72aa5229c782e6c9292f76995bd6bc6708aaf8df
Thomas Leonard <talex5@gmail.com>
2006-03-04 10:58:57 +0000

    Python2.2 doesn't have the 'logging' module. Import a dummy version instead in this case.
    git-svn-id: svn://svn.code.sf.net/p/rox/svn/trunk/ROX-Session@4471 66de3db3-b00d-0410-b41b-f4738ad19bea

---
+++ mylogging.py
@@ -0,0 +1,13 @@
+# Python 2.2 doesn't have a logging module
+import sys
+def info(*args): print args
+def warn(*args): print >>sys.stderr, args
+def debug(*args): pass
+
+class Logger:
+	def setLevel(self, level): pass
+getLogger = Logger
+
+DEBUG, INFO = 1, 2
+
+print "Imported"
